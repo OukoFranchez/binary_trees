4cb51e36bf0a53933e7ab90969214119fc0f5032
Stosh09 <stoshochieng@gmail.com>
2023-11-03 02:16:51 +0300

    function that checks if a binary tree is a valid AVL Tree

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ typedef struct node_s
 #include <stddef.h>
 
 /*tasks function prototypes*/
+int binary_tree_is_avl(const binary_tree_t *tree);
 bst_t *bst_remove(bst_t *root, int value);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_insert(bst_t **tree, int value);
